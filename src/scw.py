2362012be440f2abc788f9e76b72dc021b5a3af2
John Fitzpatrick <jfitz@computer.org>
2020-08-31 17:31:49 -0700

    Make the main window a derived class from QtWindow.

--- src/scw.py
+++ src/scw.py
@@ -1,13 +1,14 @@
 import sys
 import argparse
 from PyQt5.QtWidgets import (
-    QDesktopWidget,
     QApplication,
-    QWidget,
-    QPushButton,
-    QVBoxLayout,
+    QDesktopWidget,
     QLabel,
-    QScrollArea
+    QMainWindow,
+    QPushButton,
+    QScrollArea,
+    QVBoxLayout,
+    QWidget
 )
 from PyQt5.QtGui import QIcon, QPixmap, QPalette
 
@@ -60,7 +61,60 @@ def make_window(filename):
 
     window.setGeometry(window_left, window_top, window_width, window_height)
 
-    return window
+
+class ScwWindow(QMainWindow):
+
+    def __init__(self, filename):
+        super().__init__()
+
+        self.initUI(filename)
+
+    def initUI(self, filename):
+        desktop = QDesktopWidget()
+        avail_geom = desktop.availableGeometry()
+        desk_width = avail_geom.width()
+        desk_height = avail_geom.height()
+        print('desktop: ' + str(desk_height) + 'x' + str(desk_width))
+
+        pixmap = QPixmap(filename)
+        pixmap_height = pixmap.height()
+        pixmap_width = pixmap.width()
+
+        print('pixmap: ' + str(pixmap_height) + 'x' + str(pixmap_width))
+
+        label = QLabel()
+        label.setPixmap(pixmap)
+        scrollArea = QScrollArea()
+        scrollArea.setBackgroundRole(QPalette.Dark)
+        scrollArea.setWidget(label)
+
+        self.setCentralWidget(scrollArea)
+
+        self.statusBar().showMessage('Ready')
+
+        window_height = self.height()
+        window_width = self.width()
+
+        window_top = int(desk_height * 0.1)
+        window_left = int(desk_width * 0.1)
+
+        print('window: ' + str(window_height) + 'x' + str(window_width))
+
+        if pixmap_height > desk_height * 0.8:
+            window_height = int(desk_height * 0.8)
+
+        if pixmap_width > desk_width * 0.8:
+            window_width = int(desk_width * 0.8)
+
+        # window_height = int(pixmap_height * 0.9)
+        # window_width = int(pixmap_width * 0.9)
+        print('window: ' + str(window_height) + 'x' + str(window_width))
+
+        self.setGeometry(window_left, window_top, window_width, window_height)
+
+        # self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
 
 
 if __name__ == '__main__':
@@ -72,9 +126,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     app = QApplication([])
-    window = make_window(args.inputfile)
-    window.setWindowTitle('Select and OCR')
-    window.show()
+    ex = ScwWindow(args.inputfile)
 
     # exit and pass exit code from Qt application
     exit_code = app.exec_()
